bff6c25bf255ee8e6221cfd72daa45ed95f537e5
edorguez <edro998@gmail.com>
2025-08-20 23:19:09 -0400

    Create trip service and repository

---
+++ services/trip-service/cmd/main.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"context"
+	"log"
+	"ride-sharing/services/trip-service/internal/domain"
+	"ride-sharing/services/trip-service/internal/infrastructure/repository"
+	"ride-sharing/services/trip-service/internal/service"
+	"time"
+)
+
+func main() {
+	ctx := context.Background()
+
+	inmemRepo := repository.NewInmemRepository()
+	svc := service.NewTripService(inmemRepo)
+
+	fare := &domain.RideFareModel{
+		UserID: "42",
+	}
+
+	t, err := svc.CreateTrip(ctx, fare)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println(t)
+
+	// Keep the program running
+	for {
+		time.Sleep(time.Second)
+	}
+}
